531ab2a3ba4bb12a96e26403841714eb5ba27a96
Your Name <wowbob396@gmail.com>
2018-02-08 22:05:57 -0600

    added data

---
+++ predict.py
@@ -0,0 +1,13 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.wrappers.scikit_learn import KerasRegressor
+import pandas as pd
+
+# initializing lists to hold varying daily price values for bitcoin
+dataframeX = pd.read_csv('smallset.csv',usecols=[5,6,7,8])
+dataframeY = pd.read_csv('smallsetY.csv',usecols=[8])
+print(dataframeX.head())
+print(dataframeY.head())
+
+# creating the training model
+model = Sequential()
