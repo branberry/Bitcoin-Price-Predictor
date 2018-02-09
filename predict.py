c14fa2209442c244e374f91959c5b9e961bd6c74
Brandon <wowbob396@gmail.com>
2018-02-08 22:36:22 -0600

    created basic model

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.wrappers.scikit_learn import KerasRegressor
 import pandas as pd
+import seaborn as sns
 
 # initializing lists to hold varying daily price values for bitcoin
 dataframeX = pd.read_csv('smallset.csv',usecols=[5,6,7,8])
