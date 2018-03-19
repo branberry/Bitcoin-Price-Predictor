bdc08986dcd8ab3b40e1133b6d201a2e99e3edb3
Your Name <wowbob396@gmail.com>
2018-03-19 11:13:45 -0500

    cleaned up whole document

--- LSTMPredict.py
+++
@@ -1,9 +0,0 @@
-from keras.models import Sequential
-from keras.layers.core import Dense, Activation
-from keras.layers.recurrent import LSTM
-from keras.wrappers.scikit_learn import KerasRegressor
-from keras.optimizers import SGD
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
